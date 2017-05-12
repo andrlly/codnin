1ebe3e882ee7b3732fa5d0d6ba40b2f32c886a62
Andriy Venglinskiy <andriy.venglinskiy@gmail.com>
2017-05-12 15:31:47 +0300

    fix bugs

--- codnin.loc/wp-content/themes/twentyseventeen/inc/my-functions.php
+++ codnin.loc/wp-content/themes/twentyseventeen/inc/my-functions.php
@@ -5,7 +5,6 @@
  * Date: 08.05.17
  * Time: 11:33
  */
-
 //REGISTER NEW POST TYPE
 add_action('init', 'films_post_type');
 function films_post_type()
@@ -46,7 +45,6 @@ function films_post_type()
         )
     );
 }
-
 //REGISTER TAXONOMY
 add_action('init', 'films_register_taxonomy');
 function films_register_taxonomy()
@@ -81,34 +79,23 @@ function films_register_taxonomy()
     );
 }
 
-
 /*********************************************************/
 /******  ADD CUSTOM POST TYPE FILMS TO WOOCOMMERCE  ******/
 /*********************************************************/
-
-
 if ( class_exists( 'WooCommerce' ) ) {
-
     class WCCPT_Product_Data_Store_CPT extends WC_Product_Data_Store_CPT
     {
-
         /**
          * Method to read a product from the database.
          * @param WC_Product
          */
-
         public function read(&$product)
         {
-
-//        var_dump($product);
             $product->set_defaults();
-
             if (!$product->get_id() || !($post_object = get_post($product->get_id())) || !in_array($post_object->post_type, array('films', 'product'))) { // change birds with your post type
-                throw new Exception(__('Invalid product.', 'woocommerce'));
+                throw new Exception(__('Invalid product.', 'twentyseventeen'));
             }
-
             $id = $product->get_id();
-
             $product->set_props(array(
                 'name' => $post_object->post_title,
                 'slug' => $post_object->post_name,
@@ -121,7 +108,6 @@ if ( class_exists( 'WooCommerce' ) ) {
                 'menu_order' => $post_object->menu_order,
                 'reviews_allowed' => 'open' === $post_object->comment_status,
             ));
-
             $this->read_attributes($product);
             $this->read_downloads($product);
             $this->read_visibility($product);
@@ -129,7 +115,6 @@ if ( class_exists( 'WooCommerce' ) ) {
             $this->read_extra_data($product);
             $product->set_object_read(true);
         }
-
         /**
          * Get the product type based on product ID.
          *
@@ -150,28 +135,23 @@ if ( class_exists( 'WooCommerce' ) ) {
             }
         }
     }
-
 }
-
 add_filter( 'woocommerce_data_stores', 'woocommerce_data_stores' );
-
 function woocommerce_data_stores ( $stores ) {
     $stores['product'] = 'WCCPT_Product_Data_Store_CPT';
     return $stores;
 }
-
-add_filter('woocommerce_get_price','reigel_woocommerce_get_price',20,2);
+add_filter('woocommerce_product_get_price','reigel_woocommerce_get_price',20,2);
 function reigel_woocommerce_get_price($price,$post){
-    if ($post->post->post_type === 'films') // change this to your post type
-        $price = get_post_meta($post->id, "price_film", true); // assuming your price meta key is price
+    $post_type = get_post_type( $post->get_id() );
+    if ($post_type === 'films') // change this to your post type
+        $price = get_post_meta($post->get_id(), "price_film", true); // assuming your price meta key is price
     return $price;
 }
-
 add_filter('the_content','rei_add_to_cart_button', 20,1);
 function rei_add_to_cart_button($content){
     global $post;
     if ($post->post_type !== 'films') {return $content; }
-
     ob_start();
     ?>
     <form action="" method="post" class="add-to-cart">
@@ -180,44 +160,36 @@ function rei_add_to_cart_button($content){
         <input name="submit" type="submit" value="Add to cart" />
     </form>
     <?php
-
     return $content . ob_get_clean();
 }
-
-
 /*********************************************************/
 /*************  ADD SKYPE FIELD TO REGISTER FORM  ********/
 /*********************************************************/
-
-add_filter('add_to_cart_redirect', 'redirect_to_checkout');
+add_filter('woocommerce_add_to_cart_redirect', 'redirect_to_checkout');
 function redirect_to_checkout()
 {
-    return wc_get_checkout_url();   
-
+    return wc_get_checkout_url();
 }
-
 /*********************************************************/
 /*************  ADD SKYPE FIELD TO REGISTER FORM  ********/
 /*********************************************************/
-
+add_action( 'woocommerce_register_form_start', 'wooc_extra_register_fields', 10, 3 );
 function wooc_extra_register_fields() { ?>
+    <p class="form-row form-row-wide">
+        <label for="reg_billing_phone"><?php _e( 'Skype', 'twentyseventeen' ); ?></label>
+        <input type="text" class="input-text" name="billing_phone" id="reg_billing_phone" value="<?php if ( ! empty( $_POST['billing_phone'] ) ) esc_attr_e( $_POST['billing_phone'] ); ?>" />
+    </p>
     <p class="form-row form-row-first">
-        <label for="reg_billing_first_name"><?php _e( 'First name', 'woocommerce' ); ?><span class="required">*</span></label>
+        <label for="reg_billing_first_name"><?php _e( 'First name', 'twentyseventeen' ); ?><span class="required">*</span></label>
         <input type="text" class="input-text" name="billing_first_name" id="reg_billing_first_name" value="<?php if ( ! empty( $_POST['billing_first_name'] ) ) esc_attr_e( $_POST['billing_first_name'] ); ?>" />
     </p>
     <p class="form-row form-row-last">
-        <label for="reg_billing_last_name"><?php _e( 'Last name', 'woocommerce' ); ?><span class="required">*</span></label>
+        <label for="reg_billing_last_name"><?php _e( 'Last name', 'twentyseventeen' ); ?><span class="required">*</span></label>
         <input type="text" class="input-text" name="billing_last_name" id="reg_billing_last_name" value="<?php if ( ! empty( $_POST['billing_last_name'] ) ) esc_attr_e( $_POST['billing_last_name'] ); ?>" />
-    </p>
-    <p class="form-row form-row-wide">
-        <label for="reg_billing_phone"><?php _e( 'Skype', 'woocommerce' ); ?></label>
-        <input type="text" class="input-text" name="billing_phone" id="reg_billing_phone" value="<?php esc_attr_e( $_POST['billing_phone'] ); ?>" />
     </p>
     <div class="clear"></div>
     <?php
 }
-add_action( 'woocommerce_register_form_start', 'wooc_extra_register_fields' );
-
 /**
  * register fields Validating.
  */
@@ -228,14 +200,9 @@ function wooc_validate_extra_register_fields( $username, $email, $validation_err
     if ( isset( $_POST['billing_last_name'] ) && empty( $_POST['billing_last_name'] ) ) {
         $validation_errors->add( 'billing_last_name_error', __( '<strong>Error</strong>: Last name is required!.', 'woocommerce' ) );
     }
-    if ( isset( $_POST['billing_phone'] ) && empty( $_POST['billing_phone'] ) ) {
-        $validation_errors->add( 'billing_phone_name_error', __( '<strong>Error</strong>: Skype is required!.', 'woocommerce' ) );
-    }
     return $validation_errors;
 }
-
 add_action( 'woocommerce_register_post', 'wooc_validate_extra_register_fields', 10, 3 );
-
 /**
  * Below code save extra fields.
  */
@@ -256,25 +223,19 @@ function wooc_save_extra_register_fields( $customer_id ) {
         // Phone input filed which is used in WooCommerce
         update_user_meta( $customer_id, 'billing_phone', sanitize_text_field( $_POST['billing_phone'] ) );
     }
-
 }
 add_action( 'woocommerce_created_customer', 'wooc_save_extra_register_fields' );
-
-
 /*********************************************************/
 /*****   Redirect after woocommerce registration   *******/
 /*********************************************************/
-
 add_filter('woocommerce_registration_redirect', 'ps_wc_registration_redirect');
 function ps_wc_registration_redirect( $redirect_to ) {
     $redirect_to = home_url('/favourites');
     return $redirect_to;
 }
-
 /*********************************************************/
 /********************  POPULAR COUNT  ********************/
 /*********************************************************/
-
 function wpb_set_post_views($postID) {
     $count_key = 'wpb_post_views_count';
     $count = get_post_meta($postID, $count_key, true);
@@ -287,7 +248,6 @@ function wpb_set_post_views($postID) {
         update_post_meta($postID, $count_key, $count);
     }
 }
-
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 
 function wpb_track_post_views($post_id) {
@@ -298,5 +258,15 @@ function wpb_track_post_views($post_id) {
     }
     wpb_set_post_views($post_id);
 }
-
 add_action('wp_head', 'wpb_track_post_views');
+
+
+/**
+
+ * Add new register fields for WooCommerce registration.
+
+ *
+
+ * @return string Register fields HTML.
+
+ */
